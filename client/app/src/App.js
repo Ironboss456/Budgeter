43515dcfc5720cda9946832eeae821d3576c6215
Savit Pawar <137122831+Ironboss456@users.noreply.github.com>
2025-09-24 13:17:19 -0700

    upload button

--- client/app/src/App.js
+++ client/app/src/App.js
@@ -1,13 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [fileName, setFileName] = useState("");
+
+  const handleFileChange = (event) => {
+    if (event.target.files.length > 0) {
+      setFileName(event.target.files[0].name);
+    }
+
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+    <body>
+      <input
+				type="file"
+				accept=".csv"
+				style={{ display: "none" }}
+				id="contained-button-file"
+				onChange={handleFileChange}
+			/>
+      <div>
+        <label htmlFor="contained-button-file">
+          <Button id="Upload" variant="contained" component="span">Upload</Button>
+        </label>
+        <h1 id="Title">Budgeter</h1>  
+      </div>
+      {fileName && (
+				<div style={{ marginTop: 20 }}>
+					<p>Selected File: {fileName}</p>
+				</div>
+			)}
+    </body>
   );
 }
 
