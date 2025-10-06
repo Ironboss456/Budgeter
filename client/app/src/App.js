71355becf9e07565f8f4d171531cefead6e33c57
Savit Pawar <137122831+Ironboss456@users.noreply.github.com>
2025-10-05 22:25:40 -0700

    f

--- client/app/src/App.js
+++ client/app/src/App.js
@@ -4,18 +4,35 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
 import React, { useState } from "react";
+import Papa from 'papaparse';
 
 const App = () => {
   const [fileName, setFileName] = useState("");
+  const [csvData, setCsvData] = useState([]);
+  const [csvFile, setCsvFile] = useState(null);
 
   const handleFileChange = (event) => {
     if (event.target.files.length > 0) {
       setFileName(event.target.files[0].name);
+      const file = event.target.files[0];
+      setCsvFile(file);
+
+      if (file) {
+      Papa.parse(file, {
+        header: true,
+        complete: (results) => {
+          setCsvData(results.data);
+        },
+        error: (error) => {
+          console.error("Error parsing CSV:", error);
+        }
+      });
+    }
     }
 
   };
   return (
-    <body>
+    <body class="parent">
       <input
 				type="file"
 				accept=".csv"
@@ -30,10 +47,14 @@ const App = () => {
         <h1 id="Title">Budgeter</h1>  
       </div>
       {fileName && (
-				<div style={{ marginTop: 20 }}>
+				<div id="Confirmation">
 					<p>Selected File: {fileName}</p>
 				</div>
 			)}
+      <div>
+      <input type="file" accept=".csv" onChange={handleFileChange} />
+      {/* Render the preview here */}
+      </div>
     </body>
   );
 }
